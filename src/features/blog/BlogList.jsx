9f4fc4de4ac9dc31bc67b8c312b3b2f5f372c61c
Daniel <daniel@unimicro.no>
2024-03-05 14:53:28 +0100

    firebase

--- src/features/blog/BlogList.jsx
+++ src/features/blog/BlogList.jsx
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types'
 
-export default function BlogList ({ blogs }) {
+export default function BlogList (props) {
   const handleClick = (post) => {
     console.log('Clicked on item', post)
   }
 
   return (
   <>
-      { blogs.map((post) => (
-                <div onClick={() => handleClick(post)} className='blogCard' key={post.id}>
-                  <h1 className='blogTitle'>{post.title}</h1>
-                  <p className='content'>{post.content.slice(0, 40) + '...'}</p>
-                  <div className='blogFooter'>
-                    <p>footer scontent</p>
-                  </div>
-                </div>
+      { props.blogs.map((post) => (
+        <div onClick={() => handleClick(post)} className='blogCard' key={post.id}>
+          <h1 className='blogTitle'>{post.title}</h1>
+          <p className='content'>{post.content.slice(0, 40) + '...'}</p>
+          <div className='blogFooter'>
+            <p>footer scontent</p>
+          </div>
+        </div>
       ))}
     </>
   )
