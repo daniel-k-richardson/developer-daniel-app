51e337fc13154d0677144f88dd50826e26083d4d
Daniel <daniel@unimicro.no>
2024-03-06 07:56:34 +0100

    Update httpClient.js

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,4 +1,4 @@
-import AuthHeader from './AuthHeader'
+import AuthHeader from './AuthHeader.js'
 
 const get = async (route) => {
   const result = await fetch(import.meta.env.VITE_BACKEND_API + route, {
