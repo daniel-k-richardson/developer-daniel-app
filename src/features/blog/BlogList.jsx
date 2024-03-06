cb29c26a409a4f96598b30ee4c70a4181dc01500
Daniel <daniel@unimicro.no>
2024-03-06 07:39:25 +0100

    Revert "firebase"
    This reverts commit 9f4fc4de4ac9dc31bc67b8c312b3b2f5f372c61c.

--- src/features/blog/BlogList.jsx
+++ src/features/blog/BlogList.jsx
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types'
 
-export default function BlogList (props) {
+export default function BlogList ({ blogs }) {
   const handleClick = (post) => {
     console.log('Clicked on item', post)
   }
 
   return (
   <>
-      { props.blogs.map((post) => (
-        <div onClick={() => handleClick(post)} className='blogCard' key={post.id}>
-          <h1 className='blogTitle'>{post.title}</h1>
-          <p className='content'>{post.content.slice(0, 40) + '...'}</p>
-          <div className='blogFooter'>
-            <p>footer scontent</p>
-          </div>
-        </div>
+      { blogs.map((post) => (
+                <div onClick={() => handleClick(post)} className='blogCard' key={post.id}>
+                  <h1 className='blogTitle'>{post.title}</h1>
+                  <p className='content'>{post.content.slice(0, 40) + '...'}</p>
+                  <div className='blogFooter'>
+                    <p>footer scontent</p>
+                  </div>
+                </div>
       ))}
     </>
   )
